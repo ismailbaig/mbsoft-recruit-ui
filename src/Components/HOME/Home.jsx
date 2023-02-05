5a3893bd3816baa12963de4f4bbc81c151e6d32d
kareemabdul9700 <abdul700karim@gmail.com>
2023-02-05 21:34:33 +0530

    corsoual movement and completed

--- src/Components/HOME/Home.jsx
+++ src/Components/HOME/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Home.css';
 import homeTop from '../../images/homePage.jpg';
-import html from '../../images/html.png';
-import html2 from '../../images/html2.png';
-import facebook from '../../images/facebook.png';
+import ph1 from '../../images/ph1.avif';
+import ph2 from '../../images/ph2c.avif';
+import ph3 from '../../images/ph3avif.avif';
 
 export const Home = () => {
   return (
@@ -11,32 +11,49 @@ export const Home = () => {
       <div className="container-fluid">
         <img src={homeTop} alt="Snow" style={{ width: '100%' }} />
         {/* <div className="centered display-4">Recruitment made simple !!</div> */}
-        <div className="centered">
-          <h2>MBsoft Technolgies Pvt Ltd</h2></div>
-      </div>
+        <div className="centered ">
+          <p className='display-1 fw-bolder'>MBsoft Technolgies </p>
+          <p className='diaplay-3 fw-semibold'>Pvt Ltd</p>
+        </div>
 
-      <h1>Welcome to Home page</h1>
-      <div id="carouselExample" className="carousel slide">
-        <div className="carousel-inner">
-          <div className="carousel-item ">
-            <img src={html} className="d-block " alt="..." />
-          </div>
-          <div className="carousel-item">
-            <img src={html2} className="d-block " alt="..." />
-          </div>
-          <div className="carousel-item active">
-            <img src={facebook} className="d-block" alt="..."/>
+
+
+      </div>
+      <div className="container">
+        <div className="row">
+          <div id="carouselExample" class="carousel slide mt-4" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner c-item">
+              <div class="carousel-item active" data-bs-interval="500">
+                <img src={ph1} class="d-block w-100 c-img" alt="..." />
+              </div>
+              <div class="carousel-item c-item" data-bs-interval="500">
+                <img src={ph2} class="d-block w-100 c-img" alt="..." />
+              </div>
+              <div class="carousel-item c-item" >
+                <img src={ph3} class="d-block w-100 c-img" alt="..." />
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
       </div>
+
+
+
     </div>
+
+
   )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/react-toastify/dist/ReactToastify.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
