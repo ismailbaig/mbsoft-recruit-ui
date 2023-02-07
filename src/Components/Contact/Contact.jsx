bc900855a84ee335002effa6f9747cae6204d172
ismailbaig <ismailece2007@gmail.com>
2023-02-07 23:13:25 +0530

    contact sendemail changes.

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,59 +1,90 @@
-import React from 'react'
+import { React, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import services from '../../images/contact1.jpg';
+import contact from '../../images/contact.jpg';
+import emailjs from '@emailjs/browser';
 
 
 export const Contact = () => {
-    return ( 
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dummy', 'template_dummy', form.current, 'dummy')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+    e.target.reset();
+  };
+
+
+
+  return (
     <div>
-      <div className='container mb-5' style={{marginTop: '4rem'}}>
-      <div className="gContainerFluid image">
-     <img src={services} style={{ width: '100%' }} />
-     <div className="gCentered">
-       <h2 style={{ fontSize: '5rem' }}>Contact us</h2></div>
-   </div>
-    </div>
-    
-<div className="container">
-<div className='row'>
-<div className='col-md-6'>
-    <h1>DROP AN ENQUIRY</h1><br />
-   <Form>     
-			<Form.Control type="email" placeholder="Enter email" />
-    <br />
-			<Form.Control type="password" placeholder="Password" />
-    <br />
-			<Form.Control as="textarea" placeholder="Message" rows="3"/>
-    <br />
-		<Button className="Submit" variant="primary" type="submit">
-			SEND MESSAGE
-		</Button>
-    </Form>
-    </div>
-    <div className='col-md-6'>
-      <h1>FIND US ON </h1><br />
+      <div className='container mb-5' style={{ marginTop: '4rem' }}>
+        <div className="gContainerFluid image">
+          <img src={contact} style={{ width: '100%' }} />
+          <div className="gCentered">
+            <h2 style={{ fontSize: '5rem' }}>Contact us</h2></div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className='row'>
+          <div className='col-md-6'>
+            <h1>DROP AN ENQUIRY</h1><br />
+            <form ref={form} onSubmit={sendEmail} className=''>
+              <input type="text" placeholder='Full Name' name='user_name' required />
+              <input type="email" placeholder='Email' name='user_email' required />
+              <input type="text" placeholder='Subject' name='subject' required />
+              <textarea name='message' cols='30' rows='10'></textarea>
+              <button type='submit' className='--btn --btn-primay' >Send Message</button>
+            </form>
+            {/* <Form onSubmit={this.sendEmail}>
+              <Form.Control type="email" placeholder="Enter email" />
+              <br />
+              <Form.Control type="text" placeholder="Subject" />
+              <br />
+              <Form.Control type="text" placeholder="Phone Number" />
+              <br />
+              <Form.Control as="textarea" placeholder="Message" rows="3" />
+              <br />
+              <Button className="Submit" variant="primary" type="submit">
+                SEND MESSAGE
+              </Button>
+            </Form> */}
+          </div>
+          <div className='col-md-6'>
+            <h1>FIND US ON </h1><br />
             <h5>Address</h5>
             <svg width="200" height="20">
-                <line x1="0" y1="1" x2="200" y2="1" stroke="red" stroke-width="2" />
-              </svg>
-            <p> Kurnool,India</p>
+              <line x1="0" y1="1" x2="200" y2="1" stroke="red" strokeWidth="2" />
+            </svg>
+            <p> #41/454 SV Complex, 3rd floor,
+              <br />
+              Opp Kalyana Mandapa, S.T.B.C Road,
+              <br />
+              Kurnool - 518004</p>
             <br />
             <h5>Online Support</h5>
             <svg width="200" height="20">
-                <line x1="0" y1="1" x2="200" y2="1" stroke="red" stroke-width="2" />
-              </svg>
+              <line x1="0" y1="1" x2="200" y2="1" stroke="red" strokeWidth="2" />
+            </svg>
             <p>Coming Soon</p>
             <br />
             <h5>Contact info</h5>
             <svg width="200" height="20">
-                <line x1="0" y1="1" x2="200" y2="1" stroke="red" stroke-width="2" />
-              </svg>
+              <line x1="0" y1="1" x2="200" y2="1" stroke="red" strokeWidth="2" />
+            </svg>
             <p> Mon-Fri 9AM-6PM</p>
-           
-            </div>
+
           </div>
-           </div>
-  </div>            
+        </div>
+      </div>
+    </div>
   );
 }
--- src/Components/Samples/Samples.jsx
+++ src/Components/Samples/Samples.jsx
@@ -4,9 +4,16 @@ import './Samples.css';
 export const Samples = () => {
     return (
         <div>
-            <div className='myText heading-3'>
-                <h1>It's Friday</h1>
+            <div className='container mb-5' style={{marginTop: '20%'}}>
+                <div className='row'>
+                    <div className='col-lg-10 col-md-8 col-sm-6 col-xs-6' style={{border: '1px solid red'}}>column with 8 grids</div>
+                    <div className='col-lg-2 col-md-4 col-sm-6 col-xs-6' style={{border: '1px solid red'}}>column with 4 grids</div>
+                </div>
             </div>
+
+{/* <div className='myText heading-3'>
+                <h1>It's Friday</h1>
+            </div> */}
         </div>
 
     )
