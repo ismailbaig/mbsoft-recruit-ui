0a9c4079083e69fa7522280fd20aa5bc337252f5
muizece <muiz.ece@gmail.com>
2023-02-15 18:08:34 +0530

    Validation for the form is done

--- src/Components/HOME/Home.jsx
+++ src/Components/HOME/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Home.css';
 import homeTop from '../../images/homePage.jpg';
 import ph1 from '../../images/ph1.avif';
@@ -13,6 +13,56 @@ import homep5 from '../../images/Enquirybackgroundimage.jpg'
 
 
 export const Home = () => {
+  const initialValues = { firstName: "", lastName: "", emailId: "", phoneNo: "", services: "", message: "" }
+  const [formValues, setFormValues] = useState(initialValues)
+  const [formErrors, setFormErrors] = useState({})
+  const [isSubmit, setIsSubmit] = useState(false)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      setFormValues(initialValues);
+    }
+  }
+
+  useEffect(() => {
+
+  }, [formErrors])
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    if (!values.firstName) {
+      errors.firstName = "Username is required!";
+    }
+    if (!values.emailId) {
+      errors.emailId = "Email is required!";
+    }
+    else if (!regex.test(values.emailId)) {
+      errors.emailId = "This is not a valid email format!";
+    }
+    if (!values.phoneNo) {
+      errors.phoneNo = "Phone Number is required";
+    } else if (values.phoneNo.length < 10) {
+      errors.phoneNo = "Phone Number must be 10 characters";
+    } else if (values.phoneNo.length > 10) {
+      errors.phoneNo = "Phone Number cannot exceed more than 10 characters";
+    }
+    if (!values.message) {
+      errors.message = "Message is required";
+    } else if (values.message.length < 30) {
+      errors.message = "Message must be more than 30 characters";
+    } else if (values.message.length > 1000) {
+      errors.message = "Message  cannot exceed more than 1000 characters";
+    }
+    return errors;
+
+  };
   return (
     <div>
       <div className="gContainerFluid">
@@ -130,7 +180,7 @@ export const Home = () => {
         </div>
       </div>
       <div className="gContainerFluid gVideo mt-5 mb-5">
-        <video src={videoHome} autoPlay muted playsinline loop />
+        <video src={videoHome} autoPlay muted playsInline loop />
         {/* <div className="centered display-4">Recruitment made simple !!</div> */}
         <div className="gCenteredVideo">
           <p className='display-1 fw-bolder'>About us </p>
@@ -195,7 +245,7 @@ export const Home = () => {
         <div className="col-md-4">
           <h1 className='dispaly-4'>PORTFOLIO</h1>
           <svg width="80%" height="20">
-            <line x1="0" y1="1" x2="100%" y2="1" stroke="red" stroke-width="2" />
+            <line x1="0" y1="1" x2="100%" y2="1" stroke="red" strokeWidth="2" />
           </svg>
           <p className='fs-4'>The following is a very small representative
             sample of our vast portfolio of our services with  900+ happy clients.
@@ -221,38 +271,42 @@ export const Home = () => {
         <div className="col-md-5 homeenquiry mx-3 img-fluid" style={{ background: 'url{homep5})' }}>
           <h1 className='dispaly-4'>ENQUIRY</h1>
           <svg width="80%" height="20">
-            <line x1="0" y1="1" x2="100%" y2="1" stroke="red" stroke-width="2" />
+            <line x1="0" y1="1" x2="100%" y2="1" stroke="red" strokeWidth="2" />
           </svg>
         </div>
       </div>
       <div className="row mx-4" >
         <div className="col-md-6">
           <div className="row">
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="col-md-6 form-group mb-3 ">
-                <label for="name" style={{ float: 'left' }}>Name</label>
-                <input type="text" className="form-control" id="exampleInputName" required/>
+                <label htmlFor="name" style={{ float: 'left' }}>Name</label>
+                <input name="firstName" type="text" className="form-control " id="exampleInputName" value={formValues.firstName} onChange={handleChange} />
               </div>
+              <div className="errorMessage"><p>{formErrors.firstName}</p></div>
               <div className="col-md-6 form-group mb-3">
-                <label for="exampleInputName" style={{ float: 'left' }}>Last Name</label>
-                <input type="text" className="form-control" id="exampleInputName" />
+                <label htmlFor="exampleInputName" style={{ float: 'left' }}>Last Name</label>
+                <input name="lastName" type="text" className="form-control " id="exampleInputName" value={formValues.lastName} onChange={handleChange} />
               </div>
+
               <div className="form-group mb-3">
-                <label for="emailId" style={{ float: 'left' }}>Email address</label>
-                <input type="email" className="form-control" id="emailId" required />
+                <label htmlFor="emailId" style={{ float: 'left' }}>Email address</label>
+                <input name="emailId" className="form-control" id="emailId" value={formValues.emailId} onChange={handleChange} />
               </div>
+              <div className="errorMessage"><p>{formErrors.emailId}</p></div>
               <div className="form-group mb-3">
-                <label for="exampleInputPhone" style={{ float: 'left' }}>Phone</label>
-                <input type="tel" className="form-control" id="exampleInputPhone" />
+                <label htmlFor="exampleInputPhone" style={{ float: 'left' }}>Phone</label>
+                <input name="phoneNo" type="tel" className="form-control" id="exampleInputPhone" value={formValues.phoneNo} onChange={handleChange} />
               </div>
+              <div className="errorMessage"><p>{formErrors.phoneNo}</p></div>
               <div className="form-group mb-3">
                 <div>
-                  <label for="exampleInputPhone" style={{ float: 'left' }}>Services</label>
+                  <label htmlFor="exampleInputPhone" style={{ float: 'left' }}>Services</label>
                 </div>
                 <div className="dropdown">
                   <button className="btn dropdown-toggle"
                     style={{ textAlign: 'left', width: '100%', border: '1px solid lightgray' }}
-                    type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    name="services" type="button" data-bs-toggle="dropdown" aria-expanded="false" value={formValues.services} onChange={handleChange}>
                     Select Services
                   </button>
                   <ul className="dropdown-menu">
@@ -263,10 +317,16 @@ export const Home = () => {
                 </div>
               </div>
               <div className="form-group mb-3 mt-3">
-                <label for="exampleFormControlTextarea1" style={{ float: 'left' }}>Message</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                <label htmlFor="exampleFormControlTextarea1" style={{ float: 'left' }}>Message</label>
+                <textarea name="message" className="form-control" id="exampleFormControlTextarea1" rows="3" value={formValues.message} onChange={handleChange}></textarea>
               </div>
+              <div className="errorMessage"><p>{formErrors.message}</p></div>
               <button type="submit" className="btn btn-primary mb-5">Submit</button>
+              {Object.keys(formErrors).length === 0 && isSubmit ? (
+                <div className="success">MESSAGE SENT SUCCESSFULLY </div>
+              ) : (
+                <h5> Fill the Details</h5>
+              )}
             </form>
           </div>
         </div>
