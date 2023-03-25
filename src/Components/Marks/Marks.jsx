a5e648e6879aba06475f68904f18a0f9fd18b39d
ismailbaig <ismailece2007@gmail.com>
2023-03-25 07:16:54 +0530

    spinner added

--- src/Components/Marks/Marks.jsx
+++ src/Components/Marks/Marks.jsx
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./Marks.css";
 import data from "../../studentsdata/marksdata.json";
 import { useParams } from "react-router-dom";
+import { PropagateLoader } from "react-spinners";
 
 export const Marks = () => {
   const { rn } = useParams();
 
   const [totalMarksGot, setTotalMarksGot] = useState(0);
   const [totalMarksFor, setTotalMarksFor] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const studentmarksdetails = data.data.find((item) =>
     rn.includes(item.rollno)
@@ -25,6 +27,7 @@ export const Marks = () => {
       );
       setTotalMarksGot(marksGot);
       setTotalMarksFor(marksFor);
+      setTimeout(() => setIsLoading(false), 5000); // add 5 second delay before setting isLoading to false
     }
   }, [studentmarksdetails]);
 
@@ -51,71 +54,89 @@ export const Marks = () => {
   return (
     <section>
       <div className="container markspage">
-        <div className="row">
-          <div className="col-lg-12">
-            <div>
-              <div style={{ float: "left" }}>
-                <span>
-                  <b>Name :</b>
-                </span>{" "}
-                {studentmarksdetails.name}
-              </div>
-              <br />
-              <div style={{ float: "left" }}>
-                <span>
-                  <b>Roll No :</b>
-                </span>{" "}
-                {studentmarksdetails.rollno}
-              </div>
-              <div className="container table-responsive py-5">
-                <table className="table table-bordered table-hover">
-                  <thead className="thead-dark">
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Subject</th>
-                      <th scope="col">Marks</th>
-                      <th scope="col">Result</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {studentmarksdetails.marksdetails.map(
-                      (marksdetail, index) => (
-                        <tr key={marksdetail.subject}>
-                          <th scope="row">{index + 1}</th>
-                          <td>{marksdetail.subject}</td>
-                          <td>
-                            {marksdetail.marks_got} /{" "}
-                            {marksdetail.marks_total_per_subject}
-                          </td>
-                          <td
-                            className={
-                              marksdetail.result == "PASS" ? "pass" : "fail"
-                            }
-                          >
-                            <b>{marksdetail.result}</b>
-                          </td>
-                        </tr>
-                      )
-                    )}
-
-                    <tr>
-                      <th scope="row"></th>
-                      <td>
-                        <b>Total</b>
-                      </td>
-                      <td>
-                        <b>
-                          {totalMarksGot} / {totalMarksFor}
-                        </b>
-                      </td>
-                      <td></td>
-                    </tr>
-                  </tbody>
-                </table>
+        {isLoading ? (
+          <div className="row">
+            <div className="col-lg-12">
+              <div>
+                <div>
+                  <span>
+                    <PropagateLoader color="#4e4e4e" />
+                  </span>
+                </div>
+                <br />
+                <div>
+                  <b>Please wait while we get the data !</b>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <div className="row">
+            <div className="col-lg-12">
+              <div>
+                <div style={{ float: "left" }}>
+                  <span>
+                    <b>Name :</b>
+                  </span>{" "}
+                  {studentmarksdetails.name}
+                </div>
+                <br />
+                <div style={{ float: "left" }}>
+                  <span>
+                    <b>Roll No :</b>
+                  </span>{" "}
+                  {studentmarksdetails.rollno}
+                </div>
+                <div className="container table-responsive py-5">
+                  <table className="table table-bordered table-hover">
+                    <thead className="thead-dark">
+                      <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Subject</th>
+                        <th scope="col">Marks</th>
+                        <th scope="col">Result</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {studentmarksdetails.marksdetails.map(
+                        (marksdetail, index) => (
+                          <tr key={marksdetail.subject}>
+                            <th scope="row">{index + 1}</th>
+                            <td>{marksdetail.subject}</td>
+                            <td>
+                              {marksdetail.marks_got} /{" "}
+                              {marksdetail.marks_total_per_subject}
+                            </td>
+                            <td
+                              className={
+                                marksdetail.result == "PASS" ? "pass" : "fail"
+                              }
+                            >
+                              <b>{marksdetail.result}</b>
+                            </td>
+                          </tr>
+                        )
+                      )}
+
+                      <tr>
+                        <th scope="row"></th>
+                        <td>
+                          <b>Total</b>
+                        </td>
+                        <td>
+                          <b>
+                            {totalMarksGot} / {totalMarksFor}
+                          </b>
+                        </td>
+                        <td></td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </section>
   );
