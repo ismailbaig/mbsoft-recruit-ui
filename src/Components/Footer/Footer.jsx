78b5b3613af3fa86092e122f7107fc5e071d6908
ismailbaig <ismailece2007@gmail.com>
2023-06-25 14:28:14 +0530

    Initial : New chai minar

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Footer.css";
 import { SocialIcon } from "react-social-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import newchaiminardata from "../../data/newchaiminar.json";
 
 export const Footer = () => {
   return (
     <footer>
-      <div className="footer" style={{paddingTop: '15px'}}>
+      <div className="footer" style={{ paddingTop: "15px" }}>
         <div style={{ display: "flex", justifyContent: "space-evenly" }}>
           <div>
             <SocialIcon
@@ -32,15 +33,26 @@ export const Footer = () => {
             />
           </div>
         </div>
-        <div style={{paddingTop: '15px'}}>
+        <div style={{ paddingTop: "15px" }}>
           Gottigere post, C K palya Road, Bannerghatta Main Rd,
           Hommadevanahalli, Bengaluru, Karnataka 560083
           <br />
           <br />
-          <span id="emailId"> <FontAwesomeIcon icon={faEnvelope} /> Email:  <b>info@sanabilenglishschool.com</b></span> 
+          <span id="emailId">
+            {" "}
+            <FontAwesomeIcon icon={faEnvelope} /> Email:{" "}
+            <b>info@newchaiminar.com</b>
+          </span>
           <br />
           <br />
-          Copyright © 2023 Sanabil English Medium School.
+          <span id="telId">
+            {" "}
+            <FontAwesomeIcon icon={faPhone} /> 
+            <b>{newchaiminardata.webdata.mobilenumber}</b>
+          </span>
+          <br />
+          <br />
+          Copyright © 2023 New Chai Minar.
         </div>
       </div>
     </footer>
